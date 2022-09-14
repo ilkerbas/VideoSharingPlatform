455e361b7af49673f72c9c571b1fece0984e71d2
ilkerbas <bas.ilker@outlook.com>
2022-09-14 15:42:56 +0300

    mapping dtos and models using AutoMapper

---
+++ Services/Feed/VideoSharingPlatform.Services.Feed.API/Mapping/GeneralMapping.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using VideoSharingPlatform.Services.Feed.API.Dtos;
+using VideoSharingPlatform.Services.Feed.API.Models;
+
+namespace VideoSharingPlatform.Services.Feed.API.Mapping
+{
+    public class GeneralMapping:Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Video, VideoDto>().ReverseMap();
+            CreateMap<Feature, FeatureDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+
+            CreateMap<Video, VideoCreateDto>().ReverseMap();
+            CreateMap<Video, VideoUpdateDto>().ReverseMap();
+        }
+    }
+}
--- Services/Feed/VideoSharingPlatform.Services.Feed.API/Program.cs
+++ Services/Feed/VideoSharingPlatform.Services.Feed.API/Program.cs
@@ -1,7 +1,11 @@
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
